04f69ec3e36c4788daf0e21e4591be7b5e1e5ef4
jamessouth <jamesdanielsouth@gmail.com>
2018-04-10 18:20:51 -0500

    uploads

--- js/app.js
+++ js/app.js
@@ -3,14 +3,12 @@ const moment = require('moment');
 const bodyParser = require('body-parser');
 const Tweet = require('../config');
 const app = express();
-
 const urleParser = bodyParser.urlencoded({extended: false});
 
 moment.updateLocale('en', {relativeTime: {d: '1 day', h: '1 hour', m: '1 minute'}});
 
 app.use(express.static('css'));
 app.use(express.static('images'));
-
 app.set('view engine', 'pug');
 
 function abbrev(match, p1, p2){
@@ -18,15 +16,11 @@ function abbrev(match, p1, p2){
 }
 
 function package(p){
-	// console.log('18', p);
 	if(p.data.errors){
-		// console.log(p.data);
 		const err = new Error();
-		err.message = p.data.errors[0].message || 'Data error - please try again.2020';
-		// next(err);
+		err.message = p.data.errors[0].message || 'Data error - please try again.';
 		return Promise.reject(err);
 	} else {
-		// console.log('24');
 		return p;
 	}
 }
@@ -35,111 +29,75 @@ async function callAPI(endpoint, config){
 	try{
 		let pkg = await Tweet.get(endpoint, config);
 		return package(pkg);
-		// throw new Error();
 	} catch(e){
-		e.message = 'Data error - please try again.3535';
+		e.message = e.message || 'Data error - please try again.';
 		return Promise.reject(e);
 	}
 }
 
 function twitterTime(timeString){
 	let time = moment(timeString, 'ddd MMM DD HH:mm:ss ZZ YYYY');
-	if(time.diff(moment()) < -518400000){
+	if(time.diff(moment()) < -518400000){//6 days
 		return time.format('DD MMM');
 	} else {
 		return time.fromNow(true).replace(/(\d{1,2}) (\w+)/i, abbrev);
 	}
 }
 
-
 app.use((req, res, next) => {
-	res.masterObject = {};
 	const id = Tweet.config.access_token.split('-')[0];
 	callAPI('users/show', {user_id: id}).then(r => {
-		// console.log('line 45');
-		// '@' +
-		res.masterObject.user = [r.data.screen_name, r.data.profile_image_url_https, r.data.profile_banner_url];
+		app.locals.user = [r.data.screen_name, r.data.profile_image_url_https, r.data.profile_banner_url];
 		next();
 	}, err => {
-		// console.log('62');
 		next(err);
 	});
 });
 
-
 app.use((req, res, next) => {
   callAPI('statuses/user_timeline', {count: 5}).then(r => {
-		res.masterObject.timeline = r.data.map(x => [x.text, `@${x.user.screen_name}`, x.user.name, x.user.profile_image_url_https, x.retweet_count, x.favorite_count, twitterTime(x.created_at)]);
+		app.locals.timeline = r.data.map(x => [x.text, `@${x.user.screen_name}`, x.user.name, x.user.profile_image_url_https, x.retweet_count, x.favorite_count, twitterTime(x.created_at)]);
 		next();
 	}, err =>	next(err));
 });
 
 app.use((req, res, next) => {
-
   callAPI('friends/list', {count: 5}).then(r => {
-
-		res.masterObject.following = r.data.users.map(x => [x.name, `@${x.screen_name}`, x.profile_image_url_https]);
-		res.masterObject.numFollowed = r.data.users.length;
-		// console.log('77 ', res.masterObject.following, res.masterObject.numFollowed);
+		app.locals.following = r.data.users.map(x => [x.name, `@${x.screen_name}`, x.profile_image_url_https]);
+		app.locals.numFollowed = r.data.users.length;
 		next();
-
 	}, err => next(err));
-
 });
 
-
-
 app.use((req, res, next) => {
-	// res.masterObject = {};
-  callAPI('direct_messages/events/list', {count: 10}).then(r =>
+  callAPI('direct_messages/events/list', {count: 6}).then(r =>
 		Promise.all(r.data.events.reverse().map(async x => {
 			try{
 				let sender = await callAPI('users/show', {user_id: x.message_create.sender_id});
 				let recipient = await callAPI('users/show', {user_id: x.message_create.target.recipient_id});
 				return [recipient.data.name, recipient.data.screen_name, recipient.data.profile_image_url_https, sender.data.name, sender.data.screen_name, sender.data.profile_image_url_https, x.message_create.message_data.text, moment(x.created_timestamp, 'x').fromNow()];
-				// throw new Error();
 			} catch(e){
-				console.log(!!e.message);
-				e.message = e.message || 'Data error - please try again.9595';
+				e.message = e.message || 'Data error - please try again.';
 				return Promise.reject(e);
 			}
-		})).then(n => res.masterObject.dms = n, err => next(err)).then(() => next()), err => next(err));
+		})).then(n => app.locals.dms = n, err => next(err)).then(() => next()), err => next(err));
 });
-
-// app.use((r,e,n) => {
-//
-// 	// e.masterObject = {};
-// 	Tweet.get('application/rate_limit_status').then(x => console.log(x.data.resources.users, x.data.resources.statuses, x.data.resources.direct_messages, x.data.resources.friends));
-//
-//
-//
-//
-// 	n();
-// });
 
 app.post('/', urleParser, (req, res, next) => {
-	// console.log(req);
-	console.log('122', req.body);
-
-
-	Tweet.post('stat45645uses/update', {status: req.body.tweet}).then(null, err => next(err)).then(() => callAPI('statuses/user_timeline', {count: 5})).then(r => {
-		res.masterObject.timeline = r.data.map(x => [x.text, `@${x.user.screen_name}`, x.user.name, x.user.profile_image_url_https, x.retweet_count, x.favorite_count, twitterTime(x.created_at)]);
-		res.redirect('/');
-	}, err =>	next(err));
-
-
+	Tweet.post('statuses/update', {status: req.body.tweet}).
+		then(pkg => package(pkg), err => next(err)).
+		then(() => callAPI('statuses/user_timeline', {count: 5}), err => {
+			next(err);
+			return Promise.reject(err);
+		}).
+		then(r => {
+			app.locals.timeline = r.data.map(x => [x.text, `@${x.user.screen_name}`, x.user.name, x.user.profile_image_url_https, x.retweet_count, x.favorite_count, twitterTime(x.created_at)])
+			res.redirect('/');
+		}, err => next(err));
 });
 
-
 app.get('/', (req, res) => {
-	console.log(new Date());
-	// console.log(res.masterObject.user);
-	// console.log();
-	// console.log(res.masterObject.timeline);
-
-
-
-	const data = res.masterObject;
+	const data = app.locals;
 	const timeline = data.timeline;
 	const following = data.following;
 	const numFollowed = data.numFollowed;
@@ -148,24 +106,17 @@ app.get('/', (req, res) => {
   res.render('index', {timeline, following, numFollowed, user, dms});
 });
 
-
-
 app.use((req, res, next) => {
-
-	// console.log('line 120');
 	const err = new Error('Page Not Found...');
-	// err.status = 404;
-	// console.log(err.stack);
 	next(err);
 });
 
-// potential TODO - on error code 88 (rate limit exceeded) implement info display or countdown as to how long until the reset????
 app.use((err, req, res, next) => {
-	// console.log('line 127');
-	// res.status(err.status || 500);
-	// console.log(err.stack);
-	const user = res.masterObject.user || ['', '', ''];
+	const user = app.locals.user || ['', '', ''];
 	res.render('error', {message: err.message, user});
 });
 
 app.listen(3000);
+
+// potential TODO - on error code 88 (rate limit exceeded) implement info display or countdown as to how long until the reset????
+// potential TODO - implement socket.io????
