af4e7245091e69b0cd153ff39d79dc716a433886
jamessouth <jamesdanielsouth@gmail.com>
2018-08-10 23:36:42 -0500

    cont refactor

--- js/app.js
+++ js/app.js
@@ -25,14 +25,14 @@ function package(p){
   }
 }
 
-async function callAPI(endpoint, config){
-  try{
-    let pkg = await Tweet.get(endpoint, config);
-    return package(pkg);
-  } catch(e){
-    e.message = e.message || 'Data error - please try again.';
-    return Promise.reject(e);
-  }
+function callAPI(endpoint, config, cb){
+  console.log('requesting: ' + endpoint, Date.now());
+  cb(Tweet.get(endpoint, config));
+
+
+    // e.message = e.message || 'Data error - please try again.';
+    // return Promise.reject(e);
+
 }
 
 function getData(endpoint, config){
@@ -52,70 +52,82 @@ function twitterTime(timeString){
   }
 }
 
-app.use((req, res, next) => {
 
+
+// app.use((req, res, next) => {
+//
+//   callAPI().then(r => {
+//     // console.log(r);
+//     console.log('dms', Date.now());
+//     Promise.all(r.data.events.reverse().map(async x => {
+//       try{
+//         let sender = await callAPI('users/show', {user_id: x.message_create.sender_id});
+//         let recipient = await callAPI('users/show', {user_id: x.message_create.target.recipient_id});
+//         return [recipient.data.name, recipient.data.screen_name, recipient.data.profile_image_url_https, sender.data.name, sender.data.screen_name, sender.data.profile_image_url_https, x.message_create.message_data.text, moment(x.created_timestamp, 'x').fromNow()];
+//       } catch(e){
+//         e.message = e.message || 'Data error - please try again.';
+//         return Promise.reject(e);
+//       }
+//     })).then(n => app.locals.dms = n, err => next(err)).then(() => next()), err => next(err)});
+// });
+
+
+
+// app.use((req, res, next) => {
+//   callAPI().then(r => {
+//     console.log('rate limit', Date.now());
+//     console.log(r.data.resources.users['/users/show/:id'], r.data.resources.friends['/friends/list'], r.data.resources.statuses['/statuses/user_timeline'], r.data.resources.direct_messages['/direct_messages/events/list'], r.data.resources.application['/application/rate_limit_status']);
+//     next();
+//   }, err => next(err));
+// });
+
+
+
+
+app.use(async (req, res, next) => {
   const id = Tweet.config.access_token.split('-')[0];
-  callAPI('users/show', {user_id: id}).then(r => {
-    console.log('user', Date.now());
-    app.locals.user = [r.data.screen_name, r.data.profile_image_url_https, r.data.profile_banner_url];
-    next();
-  }, err => {
-    next(err);
+
+
+
+// getData('application/rate_limit_status'),
+
+
+
+
+  // const [user, tweet, friend, dm] =
+  await Promise.all([
+    getData('users/show', {user_id: id}),
+    getData('statuses/user_timeline', {count: 5}),
+    getData('friends/list', {count: 5}),
+    getData('direct_messages/events/list', {count: 6}),
+  ]).then(results => {
+    app.locals.user = [results[0].data.screen_name, results[0].data.profile_image_url_https, results[0].data.profile_banner_url];
+
+    app.locals.timeline = results[1].data.map(twt => [twt.text, `@${twt.user.screen_name}`, twt.user.name, twt.user.profile_image_url_https, twt.retweet_count, twt.favorite_count, twitterTime(twt.created_at)]);
+
+    app.locals.following = results[2].data.users.map(fr => [fr.name, `@${fr.screen_name}`, fr.profile_image_url_https]);
+
+    app.locals.dms = results[3].data.events.reverse();
+
+    const dmSenderID = results[3].data.events[0].message_create.sender_id;
+    const dmRecipientID = results[3].data.events[0].message_create.target.recipient_id;
+    return Promise.all([
+      getData('users/show', {user_id: dmSenderID}),
+      getData('users/show', {user_id: dmRecipientID}),
+    ]);
+  }).then(pkg => {
+    console.log(pkg);
   });
-});
-
-app.use((req, res, next) => {
-
-  callAPI('statuses/user_timeline', {count: 5}).then(r => {
-    console.log('tweets', Date.now());
-    app.locals.timeline = r.data.map(x => [x.text, `@${x.user.screen_name}`, x.user.name, x.user.profile_image_url_https, x.retweet_count, x.favorite_count, twitterTime(x.created_at)]);
-    next();
-  }, err =>  next(err));
-});
-
-app.use((req, res, next) => {
-
-  callAPI('friends/list', {count: 5}).then(r => {
-    console.log('friends', Date.now());
-    app.locals.following = r.data.users.map(x => [x.name, `@${x.screen_name}`, x.profile_image_url_https]);
-    app.locals.numFollowed = r.data.users.length;
-    next();
-  }, err => next(err));
-});
-
-app.use((req, res, next) => {
-
-  callAPI('direct_messages/events/list', {count: 6}).then(r => {
-    // console.log(r);
-    console.log('dms', Date.now());
-    Promise.all(r.data.events.reverse().map(async x => {
-      try{
-        let sender = await callAPI('users/show', {user_id: x.message_create.sender_id});
-        let recipient = await callAPI('users/show', {user_id: x.message_create.target.recipient_id});
-        return [recipient.data.name, recipient.data.screen_name, recipient.data.profile_image_url_https, sender.data.name, sender.data.screen_name, sender.data.profile_image_url_https, x.message_create.message_data.text, moment(x.created_timestamp, 'x').fromNow()];
-      } catch(e){
-        e.message = e.message || 'Data error - please try again.';
-        return Promise.reject(e);
-      }
-    })).then(n => app.locals.dms = n, err => next(err)).then(() => next()), err => next(err)});
-});
 
 
+  //
+  // console.log(app.locals.user, Date.now(), 'user');
+  // console.log(app.locals.timeline, Date.now(), 'tweet');
+  // console.log(app.locals.following, Date.now(), 'friend');
+  // console.log(app.locals.dms, Date.now(), 'dm');
+  // console.log(app.data.resources.users, Date.now(), 'app');
 
-app.use((req, res, next) => {
-  callAPI('application/rate_limit_status').then(r => {
-    console.log('rate limit', Date.now());
-    console.log(r.data.resources.users['/users/show/:id'], r.data.resources.friends['/friends/list'], r.data.resources.statuses['/statuses/user_timeline'], r.data.resources.direct_messages['/direct_messages/events/list'], r.data.resources.application['/application/rate_limit_status']);
-    next();
-  }, err => next(err));
-});
-
-
-
-
-app.use((req, res, next) => {
-
-
+  next();
 });
 
 
@@ -135,14 +147,16 @@ app.post('/', urleParser, (req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  console.log('get', Date.now());
-  const data = app.locals;
-  const timeline = data.timeline;
-  const following = data.following;
-  const numFollowed = data.numFollowed;
-  const user = data.user || ['', '', ''];
-  const dms = data.dms;
-  res.render('index', {timeline, following, numFollowed, user, dms});
+  console.log('here');
+  res.send('snot');
+  // console.log('get', Date.now());
+  // const data = app.locals;
+  // const timeline = data.timeline;
+  // const following = data.following;
+  // const numFollowed = data.numFollowed;
+  // const user = data.user || ['', '', ''];
+  // const dms = data.dms;
+  // res.render('index', {timeline, following, numFollowed, user, dms});
 });
 
 app.use((req, res, next) => {
